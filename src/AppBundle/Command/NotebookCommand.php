d64a2e75f4cde74c7756eb4e949b6767e4009dab
Andrey Bolonin <andreybolonin1989@gmail.com>
2016-12-23 20:29:42 +0200

    fixed parsing

--- src/AppBundle/Command/NotebookCommand.php
+++ src/AppBundle/Command/NotebookCommand.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Command;
 
-
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,25 +10,36 @@ use Symfony\Component\DomCrawler\Crawler;
 use Goutte\Client;
 use AppBundle\Entity\Notebook;
 
+/**
+ * Class NotebookCommand
+ *
+ * @package AppBundle\Command
+ */
 class NotebookCommand extends ContainerAwareCommand
 {
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setName('parse:notebooks')
-
             ->setDescription('Parses notebooks from Hotline.ua')
-
             ->setHelp("")
         ;
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $client = new Client();
         $crawler = $client->request('GET', 'http://hotline.ua/computer/noutbuki-netbuki/385943-883-85763-85764-85765/');
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $crawler = $crawler->filter('#catalogue > div.cell.gd > div:nth-child(2) > div > div > div.gd-img-cell.pic-tooltip > div > a > img');
+
+        $crawler = $crawler->filter('img.max-120');
         $count = 0;
 
         foreach ($crawler as $el) {
