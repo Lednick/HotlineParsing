1773b2b29b4fefc11f494737fe45875b893727c7
lednick <mazdaw4uk@icloud.com>
2016-12-23 21:25:57 +0200

    Fixed parsing

--- src/AppBundle/Command/NotebookCommand.php
+++ src/AppBundle/Command/NotebookCommand.php
@@ -1,8 +1,5 @@
 <?php
-
 namespace AppBundle\Command;
-
-
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -10,38 +7,46 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DomCrawler\Crawler;
 use Goutte\Client;
 use AppBundle\Entity\Notebook;
-
+/**
+ * Class NotebookCommand
+ *
+ * @package AppBundle\Command
+ */
 class NotebookCommand extends ContainerAwareCommand
 {
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setName('parse:notebooks')
-
             ->setDescription('Parses notebooks from Hotline.ua')
-
             ->setHelp("")
         ;
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $client = new Client();
-        $crawler = $client->request('GET', 'http://hotline.ua/computer/noutbuki-netbuki/385943-883-85763-85764-85765/');
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $crawler = $crawler->filter('#catalogue > div.cell.gd > div:nth-child(2) > div > div > div.gd-img-cell.pic-tooltip > div > a > img');
         $count = 0;
-
-        foreach ($crawler as $el) {
-            $notebook = new Notebook();
-            $notebook->setImage('http://hotline.ua' . $el->getAttribute('src'));
-            $notebook->setTitle($el->getAttribute('alt'));
-            $em->persist($notebook);
-            $count++;
+        for ($page=0; $page++<20;) {
+            $crawler = $client->request('GET', 'http://hotline.ua/computer/noutbuki-netbuki/385943-883-85763-85764-85765/?p='.$page);
+            $em = $this->getContainer()->get('doctrine')->getManager();
+            $crawler = $crawler->filter('img.max-120');
+            foreach ($crawler as $el) {
+                $notebook = new Notebook();
+                $notebook->setImage('http://hotline.ua' . $el->getAttribute('src'));
+                $notebook->setTitle($el->getAttribute('alt'));
+                $em->persist($notebook);
+                $count++;
+            }
         }
-
         $em->flush();
-
         $output->writeln([
             'Count of added records:'.$count,
         ]);
