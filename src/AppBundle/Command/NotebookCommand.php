5859af0479729b824fac3d9cb0184b216d9e5584
lednick <mazdaw4uk@icloud.com>
2016-12-26 10:54:50 +0200

    Fixed some things

--- src/AppBundle/Command/NotebookCommand.php
+++ src/AppBundle/Command/NotebookCommand.php
@@ -19,6 +19,11 @@ class NotebookCommand extends ContainerAwareCommand
             ->setHelp("");
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $client = new Client();
